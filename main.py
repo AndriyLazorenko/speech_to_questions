67152e6a6c8dc6c9f3c6f2fb1f2ea0097e0f3faf
andriy.lazorenko <andriy.lazorenko@captify.co.uk>
2017-07-05 21:21:29 +0300

    v 0.02 - added logic for processing multiple wav files and flac files. Added serialization of transcriptions to json.

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from time import sleep
 from google.cloud import speech
 from pydub import AudioSegment
 
+from os import listdir
+from os.path import isfile, join
+
+import ntpath
+
 # Cross-platform compatibility
 import platform
 
 if platform.system() == "Windows":
     AudioSegment.converter = "C:\\Users\\User\\Software\\ffmpeg-20170702-c885356-win64-static\\bin\\ffmpeg.exe"
-
-from os import listdir
-from os.path import isfile, join
-
-import ntpath
 
 RESOURCES = 'resources'
 
