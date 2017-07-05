bce53314f2bbf802d0b979b9a45b6e54385cb2df
andriy.lazorenko <andriy.lazorenko@captify.co.uk>
2017-07-05 21:18:11 +0300

    v 0.02 - added logic for processing multiple wav files and flac files. Added serialization of transcriptions to json.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import json
 import os
 
 # Imports the Google Cloud client library
@@ -6,6 +7,13 @@ from time import sleep
 
 from google.cloud import speech
 from pydub import AudioSegment
+
+from os import listdir
+from os.path import isfile, join
+
+import ntpath
+
+RESOURCES = 'resources'
 
 
 def run():
@@ -17,39 +25,51 @@ def run():
     # Instantiates a client
     speech_client = speech.Client()
 
-    # The name of the audio file to transcribe
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        'resources',
-        'russian.wav')
+    # Get all files in resources dir
+    def get_filepaths(sound_format=str()):
+        path = join(os.path.dirname(__file__), RESOURCES)
+        onlyfiles = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith("." + sound_format.lower())]
+        paths = [join(os.path.dirname(__file__), RESOURCES, f) for f in onlyfiles]
+        return paths
 
     # Convert WAV to FLAC using pydub library
-    song = AudioSegment.from_wav(file_name)
-    # Convert to mono channel as google cloud API works only with mono sound
-    song.export("resources/russian.flac", format="flac", parameters=["-ac", "1"])
+    def wav2flac(path_list):
+        for in_path in path_list:
+            song = AudioSegment.from_wav(in_path)
+            out_path = in_path[:-3] + "flac"
+            # Convert to mono channel as google cloud API works only with mono sound
+            song.export(out_path, format="flac", parameters=["-ac", "1"])
 
-    # Use new file name of flac file to transcribe
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        'resources',
-        'russian.flac')
-    # print(file_name)
+    list_filepaths = get_filepaths("WAV")
+    wav2flac(list_filepaths)
+
+    # Use flac files to transcribe
+    list_filepaths = get_filepaths("FLAC")
 
     # Loads the audio into memory
-    with io.open(file_name, 'rb') as audio_file:
-        content = audio_file.read()
-        sample = speech_client.sample(
-            content,
-            encoding='FLAC')
+    def transcribe_all_flacs(list_paths):
+        all_transcripts = dict()
+        for path in list_paths:
+            with io.open(path, 'rb') as audio_file:
+                content = audio_file.read()
+                sample = speech_client.sample(content, encoding='FLAC')
+                # Detects speech in the audio file
+                alternatives = sample.recognize('ru')
+                results = list()
+                for alternative in alternatives:
+                    print('Transcript: {}'.format(alternative.transcript))
+                    results.append(alternative.transcript)
+                file_name = ntpath.basename(path)
+                all_transcripts[file_name] = results
+        return all_transcripts
 
-    # Detects speech in the audio file
-    alternatives = sample.recognize('ru')
+    transcripts = transcribe_all_flacs(list_filepaths)
+    transcripts_filename = "transcripts.json"
+    tr_path = join(os.path.dirname(__file__), RESOURCES, transcripts_filename)
+    with open(tr_path, 'w') as outfile:
+        json.dump(transcripts, outfile)
 
-    results = list()
-    for alternative in alternatives:
-        print('Transcript: {}'.format(alternative.transcript))
-        results.append(alternative.transcript)
-    return results
+        # TODO: debug loading from json so that russian text is displayed correctly
 
 
 if __name__ == '__main__':
